a4d8f123a42cda58d1f1e7cdbc51374eaf52e10c
Yasassri Purnajith <purnajith@outlook.com>
2018-11-21 15:12:52 +0530

    bug fix

--- ChangeDataCapture/CDC.sql
+++ ChangeDataCapture/CDC.sql
@@ -65,7 +65,7 @@ GO;
 EXEC sys.sp_cdc_enable_table 
 @source_schema = N'dbo', 
 @source_name   = N'Account', 
-@role_name     = NULL
+@role_name     = NULL,
 @captured_column_list = '[first_name],[last_name]' 
 GO;
 
